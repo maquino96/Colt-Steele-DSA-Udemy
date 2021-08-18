393ce05964f7229aadd2834a583cea2430f98375
maquino96 <matthewaquino96@gmail.com>
2021-08-17 22:17:24 -0400

    wordSearch - LC

---
+++ Code-Editor-Problems/wordSearch.js
@@ -0,0 +1,44 @@
+let grid = [
+    ["C","A","A"],
+    ["A","A","A"],
+    ["B","C","D"]
+]
+
+let word = 'AAB'
+
+var exist = function(board, word) {
+    
+    let result = false;
+    let maxRows = board.length
+    let maxColumns = board[0].length
+    
+    const dfs = (r,c, str, grid) => {
+        if ( r < 0 || r >= maxRows || c < 0 || c >= maxColumns || grid[r][c] === 0) {return}
+        
+        console.log(r,c,str,result)
+        
+        if (str[0] === grid[r][c]){
+            grid[r][c] = 0;
+            if (str.length === 1) {
+                result = true;
+                return
+            }
+            str = str.slice(1) ? str.slice(1) : ''
+            dfs(r+1, c, str, grid);
+            dfs(r-1, c, str, grid);
+            dfs(r, c+1, str, grid);
+            dfs(r, c-1, str, grid);
+        }
+    }
+    
+    for ( let i = 0; i < maxRows; i++){
+        for (let j = 0; j < maxColumns; j++){
+            if (board[i][j] === word[0]) dfs(i,j,word,board)
+        }
+    }
+    
+    return result
+    
+};
+
+console.log(exist(grid, word))
